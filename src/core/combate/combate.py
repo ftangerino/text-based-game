fdc446c49d39457792f113fc8716a32f4c323bb8
ftangerino <franciscotangerino23@gmail.com>
2024-08-18 01:29:35 -0300

    Feature: Movimentação Jogador/ Geração de Inimigos para o Combate

---
+++ src/core/combate/combate.py
@@ -0,0 +1,8 @@
+from src.core.jogadores.jogador import Jogador
+from src.core.inimigos.inimigo import Inimigo
+
+class Combate:
+    def __init__(self, jogador, inimigo):
+        self.jogador = jogador
+        self.inimigo = inimigo
+
--- src/core/jogadores/jogador.py
+++ src/core/jogadores/jogador.py
@@ -7,7 +7,15 @@ class Jogador(Entidade):
         super().__init__(0, nome, nivel, hp, mp, str, dex, int, def_, luk)
         self.consumiveis = []
         self.items = []
-    
+        self.hp = hp
+        self.vida_maxima = hp
+
+    def setVida(self, vida_atual):
+        if vida_atual > self.vida_maxima:
+            self.hp = self.vida_maxima
+        else:
+            self.hp = vida_atual
+        
     def pegarConsumivel(self, consumivel):
         self.consumiveis.append(consumivel)
 
@@ -29,3 +37,31 @@ class Jogador(Entidade):
 
         playerItems = "\n".join(todosItems)
         print(playerItems)
+
+    def mover(self):
+        esquerda = "esquerda"
+        direita = "direita"
+        cima = "cima"
+        descansar = "descansar"
+        count = 0
+        while True:
+            count += 1
+            if count == 100:
+                break
+            print("""
+                Onde você deseja ir?
+                    Esquerda: 1
+                    Direita: 2
+                    Cima: 3
+                """ )
+            print(direcao := str(input("Digite a direção: ")))
+            if direcao == esquerda or direcao == direita or direcao == cima:
+                print(f"Você foi para {direcao}")
+                break
+            elif direcao == descansar: 
+                cura = self.hp * 0.1
+                self.setVida(self.hp + cura)
+                print(f"Você descansou e recuperou {cura} de vida")
+            else: print("Direção inválida")
+
+
